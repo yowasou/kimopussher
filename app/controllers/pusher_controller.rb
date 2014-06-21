9bdc614cae0ca6b6d8f8baefb3eead3742dae17b
yowasou <yowasou@yowasou-virtual-machine.(none)>
2014-06-21 19:44:18 +0900

    ss

--- app/controllers/pusher_controller.rb
+++ app/controllers/pusher_controller.rb
@@ -2,6 +2,9 @@ Pusher.url = "http://fe4a15e1a9a1df8517e5:5270dedf1c0cd9e86dcd@api.pusherapp.com
 Pusher.logger = Rails.logger
 
 class PusherController < ApplicationController
+  def index
+    @flagflat = "test"
+  end
   def create
     Pusher["channel"].trigger("event", params[:text]+"<br />")
     render :text => "success"
